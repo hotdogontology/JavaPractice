ac1fe22babc36dadfba9cfa7658f1c4b062e3c3b
Taylor Belcher <30426680+hotdogontology@users.noreply.github.com>
2023-04-10 12:30:37 -0400

    Create project and first file.

---
+++ src/HelloJava.java
@@ -0,0 +1,17 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class HelloJava {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame( "Hello, Java!");
+        frame.add(new HelloComponent());
+        frame.setSize(300,300);
+        frame.setVisible(true);
+    }
+}
+
+class HelloComponent extends JComponent {
+    public void paintComponent(Graphics g) {
+        g.drawString("Hello, Java!",125,95);
+    }
+}
